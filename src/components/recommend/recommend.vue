3e998d4bc6eb09ca23d284595d57ce3e2fb01fe5
materialcoder <1028106985@qq.com>
2017-11-15 18:55:41 +0800

    project init

---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>Recommend</div>
+</template>
+
+<script type="ecmascript-6">
+	
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+	
+</style>
---
+++ src/components/singer/singer.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>Singer</div>
+</template>
+
+<script type="ecmascript-6">
+	
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+	
+</style>
