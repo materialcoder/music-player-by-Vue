2a0fb2f20dba125ec4b923367e163448ceb81e18
materialcoder <1028106985@qq.com>
2017-11-16 20:34:16 +0800

    滚动组件抽象

---
+++ src/base/scroll/scroll.vue
@@ -0,0 +1,25 @@
+<template>
+	<div ref="wrapper">
+		<slot></slot>
+	</div>
+</template>
+
+<script type="ecmascript-6">
+	import BScroll from 'better-scroll'
+	export default {
+		props: {
+			probeType: {
+				type: Number,
+				default: 1
+			},
+			click: {
+				type: Boolean,
+				default: true
+			}
+		}
+	}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+	
+</style>
