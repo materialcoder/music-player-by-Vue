821fa8ba11d54c264c0585df055955cd37716705
materialcoder <1028106985@qq.com>
2017-11-24 17:15:59 +0800

    Add scrollbar to Scroll component

--- src/base/scroll/scroll.vue
+++ src/base/scroll/scroll.vue
@@ -37,7 +37,8 @@
 				}
 				this.scroll = new BScroll(this.$refs.wrapper, {
 					probeType: this.probeType,
-					click: this.click
+					click: this.click,
+					scrollbar: true
 				})
 
 				if(this.listenScroll) {
