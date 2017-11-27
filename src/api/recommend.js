267af40fa59e54cfbc72f8771f11dab660840bb1
materialcoder <1028106985@qq.com>
2017-11-27 13:20:35 +0800

    定义获取歌单详情方法

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -33,3 +33,29 @@ export function getDiscList() {
     return Promise.resolve(res.data)
   })
 }
+
+export function getSongList(disstid) {
+  const url = '/api/getCdInfo'
+
+  const data = Object.assign({}, commonParams, {
+    disstid,
+    g_tk: 5381,
+    uin: 0,
+    format: 'json',
+    platform: 'h5',
+    needNewCode: 1,
+    new_format: 1,
+    type: 1,
+    json: 1,
+    utf8: 1,
+    onlysong: 0,
+    song_begin: 0,
+    song_num: 100
+  })
+
+  return axios.get(url, {
+    params: data
+  }).then((res) => {
+    return Promise.resolve(res.data)
+  })
+}
