19337bda8c4d1b1063d88f2e40a8424443831563
materialcoder <1028106985@qq.com>
2017-12-01 20:57:27 +0800

    优化播放器内核bug

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -90,7 +90,7 @@
 			</div>
 		</transition>
 		<playlist ref="playlist"></playlist>
-		<audio ref="audio" :src="currentSong.url" @canplay="ready" @error="error" @timeupdate="updateTime" @ended="end"></audio>
+		<audio ref="audio" :src="currentSong.url" @play="ready" @error="error" @timeupdate="updateTime" @ended="end"></audio>
 	</div>
 </template>
 
@@ -221,6 +221,7 @@
 				}
 				if(this.playList.length === 1) {
 					this.loop()
+					return
 				} else {
 					let index = this.currentIndex + 1
 					if(index === this.playList.length) {
@@ -243,6 +244,7 @@
 				}
 				if(this.playList.length === 1) {
 					this.loop()
+					return
 				} else {
 					let index = this.currentIndex - 1
 					if(index === -1) {
@@ -283,6 +285,9 @@
 			},
 			getLyric() {
 				this.currentSong.getLyric().then((lyric) => {
+					if(this.currentSong.lyric !== lyric) {
+						return
+					}
 					this.currentLyric = new Lyric(lyric, this.handleLyric)
 					if(this.playing) {
 						this.currentLyric.play()
